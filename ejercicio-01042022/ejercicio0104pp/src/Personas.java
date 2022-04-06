1b876fd4c96bcf77c94a80377ec3bfc14ce1b28d
PabloJs13 <2021-008128-intecap.edu.gt>
2022-04-05 18:52:10 -0600

    ATP0404

---
+++ ejercicio-01042022/ejercicio0104pp/src/Personas.java
@@ -0,0 +1,73 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author HP
+ */
+public class Personas {
+    
+    private String nombre;
+    private String apellido;
+    private String direccion;
+    private String edad;
+    private String correo;
+    
+    
+    
+
+    @Override
+    public String toString() {
+        return "Personas{" + "nombre=" + nombre + ", apellido=" + apellido + ", direccion=" + direccion + ", edad=" + edad + ", correo=" + correo + '}';
+    }
+    
+    
+    
+    
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getEdad() {
+        return edad;
+    }
+
+    public void setEdad(String edad) {
+        this.edad = edad;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+    
+    
+   
+    
+}
